e0679f0ff719fba013675849044bba5ee8696443
Rithik <jainrithik2000@gmail.com>
2024-02-24 13:22:02 +0530

    setup tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='font-bold text-green-500 text-2xl'>Namaste Rithik</h1>
+    </div>
+  );
+}
+
+export default App;
