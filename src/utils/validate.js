af534564b2404909f0887e991d8ac287aba6de0b
Rithik <jainrithik2000@gmail.com>
2024-03-27 18:59:37 +0530

    updated signup form validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -13,11 +13,19 @@ export const checkSignInData = (email, password) => {
 export const checkSignUpData = (fullName, email, password) => {
     const isFullNameValid = /^[A-Z]([a-zA-Z]|\.| |')+$/.test(fullName);
     const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
-    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+    // const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
     if(!isFullNameValid) return "Invalid full name (capitalize first letter, no alphanumeric characters)"
     if(!isEmailValid) return "Email ID is not valid"
-    if(!isPasswordValid) return "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit"
+    if (password.length < 8)
+		return "Password must be at least 8 characters long.";
+	if (!/[a-z]/.test(password))
+		return "Password must contain at least 1 lowercase letter.";
+	if (!/[A-Z]/.test(password))
+		return "Password must contain at least 1 uppercase letter.";
+	if (!/\d/.test(password)) return "Password must contain at least 1 number.";
+	if (!/[^((0-9)|(a-z)|(A-Z)|\s)]/.test(password))
+		return "Password must contain at least 1 special characters.";
 
     return null;
 } 
